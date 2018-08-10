693f6385749654b8e833491dd4466573c71f30a4
Kourtnie Jenkins <KourtnieJenkins@outlook.com>
2018-08-10 17:42:22 -0400

    Updating content added to TestFileContentGrabber class.

--- BaseReleaseNoteMarkupConverter/BaseReleaseNoteMarkupConverter.Tests/TestFileContentGrabber.cs
+++ BaseReleaseNoteMarkupConverter/BaseReleaseNoteMarkupConverter.Tests/TestFileContentGrabber.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.IO;
 
 namespace BaseReleaseNoteMarkupConverter.Tests
 {
@@ -13,9 +14,34 @@ namespace BaseReleaseNoteMarkupConverter.Tests
 
         // opens a file
         // stores all content in file
+        private string filePath = "testFile.txt";
+        private string expectedFileContent = "This is a test";
+        private StreamWriter testFile;
+        private FileContentGrabber fileContentGrabber;
+
+        [SetUp]
+        public void Setup()
+        {
+            if (!File.Exists(filePath))
+            {
+                testFile = File.CreateText(filePath);
+            }
+
+            testFile.WriteLine(expectedFileContent);
+
+            fileContentGrabber = new FileContentGrabber(filePath);
+
+        }
+
+
 
         [Test]
-        public void 
+        public void GrabContentFromFile_WhenInvokedWithFileObject_StoresFileContentIntoAString()      
+        {
+            string grabbedContent = fileContentGrabber.GrabContent();
+
+            Assert.That(grabbedContent, Is.EqualTo(expectedFileContent));
+        }
 
 
     }
